3fda4c0c24614837df1ec1572d36c594e418d92c
Xristos Bisias <christosbis@gmail.com>
2022-02-03 17:02:48 +0200

    Time class

--- timly/src/main/java/io/timly/App.java
+++ timly/src/main/java/io/timly/App.java
@@ -1,9 +1,6 @@
 package io.timly;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
---
+++ timly/src/main/java/io/timly/Calculator.java
@@ -0,0 +1,13 @@
+package io.timly;
+
+import java.util.List;
+
+public class Calculator {
+    private List<Time> times;
+
+    public Calculator(List<Time> times){
+        this.times = times;
+    }
+
+
+}
