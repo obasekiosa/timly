fd2a5764c04de629d66f8f2e6e4c5e93601d88c6
Xristos Bisias <christosbis@gmail.com>
2022-02-03 23:26:18 +0200

    results window gui

--- timly/src/main/java/io/timly/MainWin.java
+++ timly/src/main/java/io/timly/MainWin.java
@@ -8,118 +8,103 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import java.time.*;
-public class MainWin extends Application {
+public class MainWin {
 
 	// launch the application
-	public void start(Stage s)
-	{
-        StackPane st= new StackPane();
-		// set title for the stage
-       // Label description_label =
-        //new Label("This is a combo box example ");
+	public static void mainWin(Stage stage) {
 
-// Weekdays
-String from[] =
-      { "0:00", "1:00", "2:00",
-                       "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00","12:00",
-					   "13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00" ,"22:00","23:00"};
-					   String to[] =
-					   {  "1:00", "2:00",
-                       "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00","12:00",
-					   "13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00" ,"22:00","23:00","0:00"};
-String timezones[] =
-      { "IST", "GMT", "EST",
-                       "UTC" };
+        Stage mainWinStage = new Stage();
+        StackPane st = new StackPane();
+        Scene sc = new Scene(st, 1080, 820);
 
-// Create a combo box
-ComboBox combo_box =
-        new ComboBox(FXCollections
-                    .observableArrayList(from));
+        mainWinStage.setMinHeight(700);
+        mainWinStage.setMinWidth(900); 
 
-                    ComboBox combo_box2 =
-        new ComboBox(FXCollections
-                    .observableArrayList(to));
+        String from[] = {   "0:00", "1:00", "2:00",
+                            "3:00", "4:00", "5:00", 
+                            "6:00", "7:00", "8:00", 
+                            "9:00", "10:00", "11:00",
+                            "12:00","13:00","14:00",
+                            "15:00","16:00","17:00",
+                            "18:00","19:00","20:00",
+                            "21:00" ,"22:00","23:00"
+                        };
+                        
+        String to[] = {     "1:00", "2:00", "3:00", 
+                            "4:00", "5:00", "6:00", 
+                            "7:00", "8:00", "9:00", 
+                            "10:00", "11:00","12:00",
+                            "13:00","14:00","15:00",
+                            "16:00","17:00","18:00",
+                            "19:00","20:00","21:00" ,
+                            "22:00","23:00","0:00"
+                        };
 
-					ComboBox combo_box3 =
-        new ComboBox(FXCollections
-                    .observableArrayList(timezones));
+        String timezones[] = { "IST", "GMT", "EST", "WAT", "UTC" };
 
+        // Create a combo box
+        ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(from));
 
-					ComboBox combo_boxn =
-        new ComboBox(FXCollections
-                    .observableArrayList(from));
+        ComboBox combo_box2 = new ComboBox(FXCollections.observableArrayList(to));
 
-                    ComboBox combo_box2n =
-        new ComboBox(FXCollections
-                    .observableArrayList(to));
+        ComboBox combo_box3 =new ComboBox(FXCollections.observableArrayList(timezones));
 
-					ComboBox combo_box3n =
-        new ComboBox(FXCollections
-                    .observableArrayList(timezones));
+        ComboBox combo_boxn = new ComboBox(FXCollections.observableArrayList(from));
+        
+        ComboBox combo_box2n = new ComboBox(FXCollections.observableArrayList(to));
 
+        ComboBox combo_box3n = new ComboBox(FXCollections.observableArrayList(timezones));
 
-					ComboBox combo_boxnn =
-        new ComboBox(FXCollections
-                    .observableArrayList(from));
+        ComboBox combo_boxnn = new ComboBox(FXCollections.observableArrayList(from));
 
-                    ComboBox combo_box2nn =
-        new ComboBox(FXCollections
-                    .observableArrayList(to));
+        ComboBox combo_box2nn = new ComboBox(FXCollections.observableArrayList(to));
 
-					ComboBox combo_box3nn =
-        new ComboBox(FXCollections
-                    .observableArrayList(timezones));
+        ComboBox combo_box3nn = new ComboBox(FXCollections.observableArrayList(timezones));
 
-					ComboBox combo_boxnx =
-        new ComboBox(FXCollections
-                    .observableArrayList(from));
+        ComboBox combo_boxnx = new ComboBox(FXCollections.observableArrayList(from));
 
-                    ComboBox combo_box2nx =
-        new ComboBox(FXCollections
-                    .observableArrayList(to));
+        ComboBox combo_box2nx = new ComboBox(FXCollections.observableArrayList(to));
 
-					ComboBox combo_box3nx =
-        new ComboBox(FXCollections
-                    .observableArrayList(timezones));
+        ComboBox combo_box3nx = new ComboBox(FXCollections.observableArrayList(timezones));
 
-	Button resultButton = new Button("Result");
-	resultButton.setFont(Font.font("verdaba", FontWeight.NORMAL, FontPosture.REGULAR, 16));
-		
-VBox v1= new VBox();
-HBox h1= new HBox();
-HBox h2= new HBox();
-HBox h3= new HBox();
-HBox h4= new HBox();
-HBox h5= new HBox();
-h1.getChildren().addAll(combo_box,combo_box2,combo_box3);
-h2.getChildren().addAll(resultButton);
-h3.getChildren().addAll(combo_boxn,combo_box2n,combo_box3n);
-h4.getChildren().addAll(combo_boxnn,combo_box2nn,combo_box3nn);
-h5.getChildren().addAll(combo_boxnx,combo_box2nx,combo_box3nx);
-v1.getChildren().addAll(h1,h3,h4,h5,h2);
-st.getChildren().addAll(v1);
-v1.setAlignment(Pos.CENTER);
-		
-		Scene sc = new Scene(st, 200, 200);
+        Button resultButton = new Button("Time Gap");
+        resultButton.setFont(Font.font("verdaba", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+                
+        VBox v1= new VBox();
+        HBox h1= new HBox();
+        HBox h2= new HBox();
+        HBox h3= new HBox();
+        HBox h4= new HBox();
+        HBox h5= new HBox();
+        h1.getChildren().addAll(combo_box,combo_box2,combo_box3);
+        h2.getChildren().addAll(resultButton);
+        h3.getChildren().addAll(combo_boxn,combo_box2n,combo_box3n);
+        h4.getChildren().addAll(combo_boxnn,combo_box2nn,combo_box3nn);
+        h5.getChildren().addAll(combo_boxnx,combo_box2nx,combo_box3nx);
+        v1.getChildren().addAll(h1,h3,h4,h5,h2);
+        st.getChildren().addAll(v1);
+        v1.setAlignment(Pos.CENTER);
+                
+        mainWinStage.setScene(sc);
+        mainWinStage.setTitle("Timly");
+        mainWinStage.show();
 
-       
-		s.setScene(sc);
-     
+        resultButton.setOnMouseClicked(new EventHandler<Event>() {
 
-		s.show();
+            @Override
+            public void handle(Event event) {
+                ResultsWin.resultsWin(stage);
+                mainWinStage.close();
+            }
+            
+        });
+
 	}
 
-	public static void main(String args[])
-	{
-		// launch the application
-		launch(args);
-	}
-
-    public static void mainWin(Stage stage) {
-    }
 }
--- timly/src/main/java/io/timly/ResultsWin.java
+++ timly/src/main/java/io/timly/ResultsWin.java
@@ -1,13 +1,30 @@
 package io.timly;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ResultsWin {
+
+    public static String timezone;
+    public static String timeGap;
+    public static String convTimeGap;
     
     public static void resultsWin(Stage stage) {
         Stage resultsStage = new Stage();
@@ -17,20 +34,76 @@ public class ResultsWin {
         resultsStage.setMinHeight(700);
         resultsStage.setMinWidth(900);  
 
-        VBox vbox = new VBox();
+        VBox mainVBox = new VBox();
+        VBox convVBox = new VBox();
+        HBox convHBox = new HBox();
 
-        /*Label label = new Label("some text");
-        TextField textField = new TextField();
+        timezone = "UTC";
+        timeGap = "some TimeGap returned from a backend method";
+        convTimeGap = "select a timezone to make a convertion to";
 
-        stackPane.getChildren().addAll(vbox);
-        vbox.getChildren().addAll(label, textField);*/
+        Label mainLabel = new Label("Overlaping time gaps in timezone " + timezone);
+        mainLabel.setFont(Font.font("Courier New", FontWeight.BOLD, FontPosture.REGULAR, 28));
 
-        vbox.setSpacing(20);
+        Label resultslabel = new Label(timeGap);
+        resultslabel.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+        
+        Button convertButton = new Button("Convert");
+        convertButton.setFont(Font.font("verdaba", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+        convertButton.setPrefSize(100.0, 40.0);
+        convertButton.setTextAlignment(TextAlignment.CENTER);
 
-       // textField.setText(label.getText());
+        Label toLabel = new Label("To");
+        toLabel.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+
+        ComboBox<String> timezoneCombo = new ComboBox<String>();
+        timezoneCombo.getItems().addAll("IST", "GMT", "EST", "WAT", "UTC");
+        timezoneCombo.getSelectionModel().select(4);
+        timezoneCombo.setStyle("-fx-font-size: 16");
+
+        Label convertResLabel = new Label(convTimeGap);
+        convertResLabel.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+
+
+        stackPane.getChildren().addAll(mainVBox);
+        mainVBox.getChildren().addAll(mainLabel, resultslabel ,convVBox);
+        convVBox.getChildren().addAll(convHBox, convertResLabel);
+        convHBox.getChildren().addAll(convertButton, toLabel, timezoneCombo);
+
+        mainVBox.setSpacing(40);
+        convVBox.setSpacing(20);
+        convHBox.setSpacing(15);
+
+        mainVBox.setAlignment(Pos.CENTER);
+        convVBox.setAlignment(Pos.CENTER);
+        convHBox.setAlignment(Pos.CENTER);
+
+        stackPane.setPadding(new Insets(10, 10, 10, 10));
 
         resultsStage.setTitle("Timly Results");
         resultsStage.setScene(scene);
         resultsStage.show();
+
+        convertButton.setOnMouseClicked(new EventHandler<Event>() {
+
+            @Override
+            public void handle(Event event) {
+                if(timezone.equals(timezoneCombo.getValue())){
+                    Alert alert = new Alert(AlertType.WARNING);
+	                alert.initModality(Modality.APPLICATION_MODAL);
+	                alert.initOwner(resultsStage);
+	                alert.setTitle("Timezone warning");
+	                alert.setHeaderText("Timezones are the same");
+	                alert.setContentText("You need to select a different timezone");
+	                alert.showAndWait();
+                }
+                else{
+                    convTimeGap = "some convTimeGap returned from a backend method";
+                    convertResLabel.setText(convTimeGap);
+                }
+
+            }
+            
+        });
     }
 }
