ac098d0ee835fe0ab8a53b0d235345d8fc9369ff
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-02-28 15:48:07 +0100

    Add Spring Web Server

--- src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module io.timly {
-    requires javafx.controls;
-    exports io.timly;
-}
---
+++ src/test/java/io/timly/timly/TimlyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.timly.timly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
